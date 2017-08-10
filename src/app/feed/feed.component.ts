a6131c7bd8f9072ae543a3d5a55b48bc01954086
garethgd <garethgdunne@gmail.com>
2017-08-10 20:44:58 +0100

    FireBase Connected for posting messages

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
+import { ChatService } from '../services/chatservice.service';
+import { Observable } from 'rxjs/Observable'
+import { ChatMessage } from '../models/chat-message.model';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
-
-  constructor() { }
+export class FeedComponent implements OnInit, OnChanges {
+  feed: FirebaseListObservable<ChatMessage[]>;
+  constructor(private chat: ChatService) { }
 
   ngOnInit() {
+    this.feed = this.chat.getMessages()
+  }
+
+  ngOnChanges() {
+    this.feed = this.chat.getMessages();
   }
 
 }
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChatService } from '../services/chatservice.service';
+import { AuthService } from '../services/auth.service';
+import { ChatMessage } from '../models/chat-message.model';
 
 @Component({
   selector: 'app-message',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
-
+  @Input() chatMessage: ChatMessage
   constructor() { }
 
-  ngOnInit() {
+  userEmail: string;
+  userName: string;
+  messageContent: string;
+  timeStamp: Date;
+
+  ngOnInit(chatMessage = this.chatMessage) {
+    this.messageContent = chatMessage.message;
+    this.userEmail = chatMessage.email;
+    this.timeStamp = chatMessage.timeSent;
+    this.userName = chatMessage.userName;
   }
 
 }
