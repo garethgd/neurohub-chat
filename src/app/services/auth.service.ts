469d89402f7b279b4a343f219bd6ba1bbc547c3d
garethgd <garethgdunne@gmail.com>
2017-08-30 21:16:12 +0100

    Added User Authentication - Styles for Sign up form as well as authenticaion service methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,71 @@
+import { passBoolean } from 'protractor/built/util';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
+import { ChatService } from '../services/chatservice.service';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { ChatMessage } from '../models/chat-message.model';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class AuthService {
+  private user: Observable<firebase.User>;
+  private authState: any;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private db : AngularFireDatabase, private router:Router)
+    {
+      this.user = afAuth.authState
+    }
+
+    get currentUserId ():string{
+      return this.authState !== null ? this.authState.uid : '';
+    }
+
+    login(email:string , password : string)
+    {
+      return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((resolve) => {
+        const status ='online';
+        this.setUserStatus(status);
+        this.router.navigate(['chat'])
+      })
+    }
+
+    signUp(email: string, password : string, displayName : string)
+    {
+      return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+          this.authState = user;
+          const status = 'online';
+          this.setUserData(email, displayName, status)
+        })
+        .catch(error => console.log(error));
+    }
+
+    setUserData(email: string, displayName: string, status: string): void{
+      const path = `users/${this.currentUserId}`
+      const data = {
+        email: email,
+        displayName: displayName,
+        status: status
+      }
+
+     
+  
+      this.db.object(path).update(data)
+      .catch(error => console.log(error))
+    }
+
+      setUserStatus(status: string): void{
+      const path = `users/${this.currentUserId}`
+      const data = {
+        status: status
+      }
 
 }
+}
+
